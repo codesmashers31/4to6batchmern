a213acb56cf5437d4f35b7b549b091565a87cfbb
bala <balasudhan17@gmail.com>
2026-03-24 20:19:13 +0530

    updated"

---
+++ React/CreatePhase/Day1/craproject/src/App.js
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-blue-700 p-3 text-white font-bold text-2xl text-center">
+the react
+    </div>
+
+ <div>
+the react cra
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ React/CreatePhase/Day2/componentsprocess/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import Data from './components/Data'
+// import HomePage from './pages/HomePage'
+const App = () => {
+  return (
+   <Data/>
+  )
+}
+
+export default App
