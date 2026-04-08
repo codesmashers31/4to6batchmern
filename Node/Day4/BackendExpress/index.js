133feb025a4b70e91363b8bc38a3317d8bc9c1b4
bala <bala>
2026-04-08 17:35:57 +0530

    updated

---
+++ Node/Day4/BackendExpress/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+
+const PORT = process.env.PORT || 3000
+
+
+
+
+app.listen(PORT,()=>{
+
+    console.log(`Server Runninng on http://localhost:${PORT}`);
+    
+
+})
